c510bb0299d2f68a6708d57c646bbf758f14c9ae
Suny Xie <inbox@xieziming.com>
2017-06-26 21:24:44 +0800

    update tap service

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,1 @@
---INSERT INTO EXECUTION(PATH, TEST_CASE, STATUS, RESULT) VALUES ("suny.test", "t1", "Started", "");
---INSERT INTO EXECUTION(PATH, TEST_CASE, STATUS, RESULT) VALUES ("suny.test", "t2", "Completed", "Pass")
 SELECT 1
